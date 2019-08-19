129b3a4f774edad77236c4ae5d02d37b112723e8
ltf <893305119@qq.com>
2019-08-19 11:00:47 +0800

    xiugai tianqi

--- app/src/main/java/com/hskj/meettingsys/K780/K780Utils.java
+++ app/src/main/java/com/hskj/meettingsys/K780/K780Utils.java
@@ -7,7 +7,7 @@ public class K780Utils {
 	public static final String host = "http://api.k780.com/?app=weather.today";
 	public static final String weaid = "1";
 	public static final String APPKEY = "42583";
-	public static final String SIGN = "7cb438dc230ebed6e14d34b9a5f0cfaa";
+	public static final String SIGN = "fc72d21319ae9e52b8830f9dff9332c9";
 	public static final String WEATHER_URL = host
 			+ "&weaid="+ weaid
 			+ "&appkey=" + APPKEY
